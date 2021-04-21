32902d1a43338bc50c534b82e661fd1356053b2c
Pericles001 <periclesadjovi@utlook.fr>
2021-04-21 21:54:27 +0100

    tests in js file

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const keys = calculator.querySelector('.calculator__keys');
 
 keys.addEventListener('click', e=>{
     if(e.target.matches('button')){
-        
+        const key = e.target;
+        const action = key.dataset.action;
+        if(!action){
+            console.log('number key!')
+        }
+
+        if(action==='add' || action==='substract' || action==='multiply' || action==='divide'){
+            console.log('operator key!')
+        }
     }
 })
