b3b7d48e39760986efc654a943c7aede6cc1ab1a
Pericles001 <periclesadjovi@utlook.fr>
2021-04-22 00:25:03 +0100

    initial zero

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
-keys.addEventListener('click', e=>{
-    if(e.target.matches('button')){
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
         const key = e.target;
         const action = key.dataset.action;
-        if(!action){
-            console.log('number key!')
-        }
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
 
-        if(action==='add' || action==='substract' || action==='multiply' || action==='divide'){
+        if (!action) {
+          if(displayedNum==='0'){
+              display.textContent = keyContent
+          }
+        }
+        /*
+     
+
+        if (action === 'add' || action === 'substract' || action === 'multiply' || action === 'divide') {
             console.log('operator key!')
         }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+        }
+        if (action === 'clear') {
+            console.log('clear key!')
+        }
+        if (action === 'calculate') {
+            console.log('equal key!')
+        }
+        */
     }
 })
